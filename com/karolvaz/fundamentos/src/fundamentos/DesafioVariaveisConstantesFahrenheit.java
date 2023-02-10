75a8fca9b6324cdcfd125eaa7e8654409ea958ba
Karol Vaz <karolinelemesvaz@gmail.com>
2023-02-09 23:19:11 -0300

    Atualizacao exercicios java

---
+++ com/karolvaz/fundamentos/src/fundamentos/DesafioVariaveisConstantesFahrenheit.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioVariaveisConstantesFahrenheit {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // (ºF-32) x 5/9 = ºC
+        double fahrenheit;
+        double celsius;
+
+        final int NUM = 32;
+        final double DIV = 5.0 / 9.0;
+
+        System.out.print("Informe a temperatura: ");
+        fahrenheit = scanner.nextInt();
+
+        celsius = (fahrenheit - NUM) * DIV;
+        System.out.print(celsius);
+
+        scanner.close();
+    }
+}
