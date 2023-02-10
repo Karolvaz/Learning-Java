64ee7286b97a459a734b9d804829fbe752d63bbf
Karol Vaz <karolinelemesvaz@gmail.com>
2023-02-09 23:36:12 -0300

    Atualizacao exercicios java

---
+++ com/karolvaz/fundamentos/src/fundamentos/VariaveisConstantes.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class VariaveisConstantes {
+    public static void main(String[] args) {
+
+        int numeroInt = 8;
+        long numeroLong = 4L;
+        double numeroDouble = 2.5;
+
+        char caracterChar = 'a';
+        String palavraString = "Karol";
+        boolean isVerdadeiro = true;
+        boolean isFalse = false;
+
+        System.out.println(numeroInt);
+        System.out.println(numeroLong);
+        System.out.println(numeroDouble);
+        System.out.println(caracterChar);
+        System.out.println(palavraString);
+        System.out.println(isVerdadeiro);
+        System.out.println(isFalse);
+    }
+}
