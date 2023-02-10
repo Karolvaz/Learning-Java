ffbdcba7f110645c7b069be8bff5bf736bf5ec11
Karol Vaz <karolinelemesvaz@gmail.com>
2023-02-09 23:05:32 -0300

    Atualizacao exercicios java

--- com/karolvaz/fundamentos/src/fundamentos/Console.java
+++ com/karolvaz/fundamentos/src/fundamentos/Console.java
@@ -7,8 +7,10 @@ public class Console {
 
         System.out.print("Bom");
         System.out.print(" dia!\n");
+
         System.out.println("Bom");
         System.out.println("dia");
+
         System.out.printf("Megasena: %d %d %d %d %d %d %n", 1, 2, 3, 4, 5,6);
         System.out.printf("Salario: %.1f%n", 1234.5678);
         System.out.printf("Nome: %s%n", "João");
---
+++ com/karolvaz/fundamentos/src/fundamentos/DesafioScanner.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+import java.util.Scanner;
+public class DesafioScanner{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+        int idade;
+
+        System.out.print("Digite seu nome: ");
+        nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        idade = scanner.nextInt();
+
+        System.out.println("Seu nome é " + nome + " e você possui " + idade + " anos de idade");
+
+        scanner.close();
+    }
+}
