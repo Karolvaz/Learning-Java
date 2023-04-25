fa4dca4e525d83346dffd498784011960ffd92b8
Karoline <karolinelemesvaz@gmail.com>
2023-04-25 16:59:58 -0300

    Finalização do capitulo Classe e Metodos

---
+++ src/classeMetodos/PrimeiroDesafio.java
@@ -0,0 +1,12 @@
+package classeMetodos;
+
+public class PrimeiroDesafio {
+    int a = 3; // não pode mexer aqui!!!
+    public static void main(String[] args) {
+
+        PrimeiroDesafio p = new PrimeiroDesafio();
+        System.out.println(p.a);
+
+    }
+}
+
---
+++ src/classeMetodos/desafio/Comida.java
@@ -0,0 +1,15 @@
+package classeMetodos.desafio;
+
+public class Comida {
+
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso){
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+
+
+}
